5f7c55189d1321a263ef142ce7ecad3186a45f94
Roman Bishof <roman@octopus-app.com>
2022-03-23 09:18:20 +0200

    update store

--- patrol-route/src/redux/store.jsx
+++ patrol-route/src/redux/store.jsx
@@ -1,9 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 import patrolsReducer from "./patroslSlice";
 
 export default configureStore({
     reducer: {
         patrols: patrolsReducer,
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    })
 })
