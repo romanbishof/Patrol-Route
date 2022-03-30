6450b58cc2c3fc28e1d177c6e4318c217b7fca5c
Roman Bishof <roman@octopus-app.com>
2022-03-30 16:29:39 +0300

    added popup overlay in map

--- patrol-route/src/components/SetRoute/SetRoute.jsx
+++ patrol-route/src/components/SetRoute/SetRoute.jsx
@@ -1,11 +1,11 @@
-import { Box, Button, FormControl, FormControlLabel, InputLabel, MenuItem, Select, Slider, Stack, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material'
-import React, { useEffect, useState } from 'react'
+import { Box, Button, Card, CardContent, CardHeader, FormControl, FormControlLabel, InputLabel, MenuItem, Paper, Select, Slider, Stack, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@mui/material'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { postRoutesAsync, setRoutePlans } from '../../redux/patroslSlice'
 import { v4 as uuidv4 } from 'uuid'
 import { fromLonLat } from 'ol/proj'
-import { Overlay } from 'ol'
+import Overlay from 'ol/Overlay';
 
 
 function SetRoute() {
@@ -13,13 +13,17 @@ function SetRoute() {
     // const patrols = useSelector((state) => state.patrols)
     const [routeName, setRouteName] = useState('')
     const [routePoint, setRoutePoints] = useState([])
-    const [waitforSeconds, setWaitforSeconds] = useState('')
     const [date, setDate] = useState('')
     const [pointNumber, setPointNumber] = useState(1)
     const [interval, setInterval] = useState(10)
-    const [open, setOpen] = useState(false)
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const popup = useRef();
+
+    const [camera, setCamera] = useState('No Camera')
+    const [xenon, setXenon] = useState('No Xenon')
+    const [devices, setDevices] = useState([])
+    const [coordinates, setCoordinates] = useState()
 
     const hendleSaveRoute = (e) => {
         e.preventDefault();
@@ -72,6 +76,41 @@ function SetRoute() {
         console.log(e.target.value);
         setInterval(e.target.value)
     }
+    const handleDevices = () => {
+
+        let templatePoint = {
+            Id: uuidv4(),
+            Name: `Point No. ${pointNumber}`,
+            Latitude: coordinates[1].toString(),
+            Longitude: coordinates[0].toString(),
+            WaitforSeconds: interval,
+            Devices: devices
+        }
+
+        setRoutePoints(oldpoints => [...oldpoints, templatePoint])
+        setCamera('No Camera');
+        setXenon('No Xenon');
+
+        const _overlays = window.map.getOverlays()
+        
+        // _overlays.forEach((item)=> console.log(item))
+        _overlays.removeAt(0)
+    }
+    const handleCamera = (e) => {
+        if (e.target.value === 'No Camera') {
+            console.log('user dident choose camera');
+        } else {
+            setCamera(e.target.value)
+        }
+    }
+
+    const handleXenon = (e) => {
+        if (e.target.value === 'No Xenon') {
+            console.log('user dident choose xenon');
+        } else {
+            setXenon(e.target.value)
+        }
+    }
 
     useEffect(() => {
         // adding option to draw on the map
@@ -84,23 +123,31 @@ function SetRoute() {
         // )
         // window.map.addInteraction(draw)
 
-
+        const overlay = new Overlay({
+            element: popup.current,
+            autoPan:true,
+            id: 'popupMenu'
+        })
         // get coordinates of the map by click
-        window.map.on("singleclick", (e) => {
-  
-            let template = {
-                Id: uuidv4(),
-                Name: `Point No. ${pointNumber}`,
-                Latitude: e.coordinate[1].toString(),
-                Longitude: e.coordinate[0].toString(),
-                WaitforSeconds: interval,
-                Devices: []
-            }
-            // updating state of component
-            setRoutePoints(oldpoints => [...oldpoints, template])
+        window.map.on("click", (e) => {
+
+            overlay.setPosition(e.coordinate);
+            window.map.addOverlay(overlay)
+            setCoordinates(e.coordinate)
+            // let template = {
+            //     Id: uuidv4(),
+            //     Name: `Point No. ${pointNumber}`,
+            //     Latitude: e.coordinate[1].toString(),
+            //     Longitude: e.coordinate[0].toString(),
+            //     WaitforSeconds: interval,
+            //     Devices: []
+            // }
+            // // updating state of component
+            // setRoutePoints(oldpoints => [...oldpoints, template])
         })
 
     }, [])
+
 
     return (
 
@@ -188,7 +235,51 @@ function SetRoute() {
                 </Stack>
 
             </Box>
-            <div id='popup'></div>
+            <div ref={popup} id='popup'>
+
+                <Paper elevation={6}
+                component='form'>
+
+                        <CardHeader title='Choose Devices' />
+                        <CardContent>
+                            <Stack spacing={2} direction={'column'}>
+                                <FormControl required={true} fullWidth>
+                                    <InputLabel id='CameraLabelId'>Camera</InputLabel>
+                                    <Select
+                                        labelId='CameLabelId'
+                                        label='Camera'
+                                        defaultValue=''
+                                        // onChange={(e) => { handleCameraChange(e, route.Id) }}
+                                        onChange={handleCamera}
+                                    >
+                                        <MenuItem value='No Camera'>No Camera</MenuItem>
+                                        <MenuItem value='c968288d-5f85-40b7-8b38-5ae9a3fc5670'>APA-MEO-001 46.3</MenuItem>
+                                        <MenuItem value='d0fbdcd9-1886-4d78-8e14-f3b7a6eb57db'>APA-WT1-SEO 46.4</MenuItem>
+                                        <MenuItem value='c34129c4-fbcd-4644-b225-43f2be700224'>APA-WT2-SEO 46.5</MenuItem>
+
+                                    </Select>
+                                </FormControl>
+                                <FormControl required={true} fullWidth>
+                                    <InputLabel id='XenonLabelId'>Xenon</InputLabel>
+                                    <Select
+                                        labelId='XenonLabelId'
+                                        label='Xenon'
+                                        defaultValue=''
+                                        // onChange={(e) => { handleXenonChange(e, route.Id) }}
+                                        onChange={handleXenon}
+                                    >
+                                        <MenuItem value='No Xenon'>No Xenon</MenuItem>
+                                        <MenuItem value='38242558-4403-4cf9-8d38-bf209880836f'>APA-XEN-001</MenuItem>
+                                    </Select>
+                                </FormControl>
+
+                                <Button variant='contained' onClick={handleDevices}>Save / Close</Button>
+                            </Stack>
+
+                        </CardContent>
+                </Paper>
+
+            </div>
         </div>
     )
 }
